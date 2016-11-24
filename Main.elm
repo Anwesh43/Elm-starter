4c04fc139f565bdab48ba3103ee85430c5c89331
Anwesh43 <anweshthecool0@gmail.com>
2016-11-24 18:43:14 +0530

    Two argument function

--- Main.elm
+++ Main.elm
@@ -1,5 +1,7 @@
 module Main exposing (..)
 import Html exposing (text)
-ask :String -> String
-ask thing = "Hey "++thing++" how are you?"
-main = text <| ask "John"
+ask :String -> String -> String
+ask thing place = "Hey "++thing++" how are you? How is the weather in "++place++"? "
+sayBye :String -> String
+sayBye phrase = phrase++"Ok Bye!!"
+main = text <| sayBye <| ask "John" "China"
