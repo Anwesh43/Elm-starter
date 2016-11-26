4434620f0af4a9726eac3d54719fb331c861dcd4
Anwesh43 <anweshthecool0@gmail.com>
2016-11-26 22:37:03 +0530

    Creating alias of the record and taking the record as a argument in the function

--- RecordDemo.elm
+++ RecordDemo.elm
@@ -2,7 +2,9 @@ module RecordDemo exposing (..)
 import Html exposing (text)
 greetFromName :String -> String
 greetFromName name = "hello "++name
-person = {name="Anwesh",age = 23}
-
-displayPerson = "My name is "++person.name ++" "++ toString person.age
-main = text <| displayPerson
+type alias Person = {name:String,age:Int}
+person1 = {name = "Brin",age = 24}
+person2 = {name="Anwesh",age = 25}
+displayPerson : Person -> String
+displayPerson person = "name is "++person.name ++" age is "++ toString person.age
+main = text <| displayPerson person1 ++ " "++displayPerson person2
