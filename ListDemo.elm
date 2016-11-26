f3d3cd2c765d4e796de8814ad2d69c3166b67469
Anwesh43 <anweshthecool0@gmail.com>
2016-11-26 22:58:30 +0530

    Mapping list of records to individual variables

--- ListDemo.elm
+++ ListDemo.elm
@@ -3,5 +3,6 @@ import Html exposing (text)
 import List
 type alias Person = {name:String,age:Int}
 people = [{name="A1",age=20},{name="A2",age=21},{name="A3",age=22}]
-
-main = text <| toString people
+peopleToNames :List Person -> List String
+peopleToNames persons = List.map (\person -> person.name) persons
+main = text <| toString <| peopleToNames people
