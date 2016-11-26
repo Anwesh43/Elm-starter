75ccc2c63895e5ea8ac65bf938275c4749f738a5
Anwesh43 <anweshthecool0@gmail.com>
2016-11-27 00:42:27 +0530

    Html Rendering

---
+++ RenderHtmlDemo.elm
@@ -0,0 +1,11 @@
+module RenderHtmlDemo exposing (..)
+import Html exposing (..)
+import Html.Attributes  exposing (..)
+import List
+type alias Student = {name:String,subject:String}
+studentToString :Student -> String
+studentToString student = student.name++","++student.subject
+students = [{name="A1",subject="Maths"},{name="A2",subject="Science"},{name="A3",subject="Physics"}]
+renderStudent student = li [style [("background","green")]] [text  <| studentToString student]
+renderStudents students = div [style [("border","1px solid black"),("width","140px"),("height","100px")]] [ul [] (List.map renderStudent students)]
+main = renderStudents students
