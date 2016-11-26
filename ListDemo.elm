46f2f822492397ca24ffc1907c03e5732cb82657
Anwesh43 <anweshthecool0@gmail.com>
2016-11-26 22:56:41 +0530

    List of records

--- ListDemo.elm
+++ ListDemo.elm
@@ -1,4 +1,7 @@
 module ListDemo exposing (..)
 import Html exposing (text)
-list = ["one","two","three"]
-main = text <| toString list
+import List
+type alias Person = {name:String,age:Int}
+people = [{name="A1",age=20},{name="A2",age=21},{name="A3",age=22}]
+
+main = text <| toString people
