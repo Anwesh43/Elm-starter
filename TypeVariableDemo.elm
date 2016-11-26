353f7cbb3e0fb44f2e82693ebecdca84c2c19043
Anwesh43 <anweshthecool0@gmail.com>
2016-11-27 01:18:49 +0530

    testing type variable for numbers array

--- TypeVariableDemo.elm
+++ TypeVariableDemo.elm
@@ -7,3 +7,5 @@ fruits = [{name="Apple"},{name="Orange"},{name="Banana"}]
 numbers = [1,2,3,4,5,6]
 printObjects :thing -> Html msg
 printObjects thing = li [][text <|toString thing]
+renderObjects h1Title things = div [style [("width","200px"),("height","200px")]][h1 [][text h1Title],ul [](List.map printObjects things)]
+main = renderObjects "numbers" numbers
