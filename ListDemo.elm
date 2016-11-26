19bd24ae384deeb8e6d305a361c1d8ad0884b9e8
Anwesh43 <anweshthecool0@gmail.com>
2016-11-26 23:11:23 +0530

    function for finding something

--- ListDemo.elm
+++ ListDemo.elm
@@ -5,4 +5,10 @@ type alias Person = {name:String,age:Int}
 people = [{name="A1",age=20},{name="A2",age=21},{name="A3",age=22}]
 peopleToNames :List Person -> List String
 peopleToNames persons = List.map (\person -> person.name) persons
-main = text <| toString <| peopleToNames people
+findPeople :String-> List Person -> Maybe Person
+findPeople name people = List.foldl (\peep memo -> case memo of
+                                  Just _ -> memo
+                                  Nothing -> if peep.name == name then
+                                              Just peep
+                                              else Nothing) Nothing people
+main = text <| toString <| findPeople "A4" people
