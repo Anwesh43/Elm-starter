9eb1c573bcdda12c18b32977b6d14e011ab51e13
Anwesh43 <anweshthecool0@gmail.com>
2016-11-27 01:19:28 +0530

    Testing for fruits

--- TypeVariableDemo.elm
+++ TypeVariableDemo.elm
@@ -8,4 +8,5 @@ numbers = [1,2,3,4,5,6]
 printObjects :thing -> Html msg
 printObjects thing = li [][text <|toString thing]
 renderObjects h1Title things = div [style [("width","200px"),("height","200px")]][h1 [][text h1Title],ul [](List.map printObjects things)]
-main = renderObjects "numbers" numbers
+-- main = renderObjects "numbers" numbers
+main = renderObjects "fruits" fruits
